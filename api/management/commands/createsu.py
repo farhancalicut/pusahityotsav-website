4e9db18fc359d177e055482dacc354ef94f1075b
farhancalicut <muhammedfarhant6@gmail.com>
2025-09-17 04:05:40 +0530

    Add custom command to create superuser

--- api/management/commands/createsu.py
+++ api/management/commands/createsu.py
@@ -1,20 +1,22 @@
 # In api/management/commands/createsu.py
 
-import os
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    help = 'Creates a superuser from environment variables'
+    help = 'Creates a superuser'
 
     def handle(self, *args, **options):
         User = get_user_model()
-        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
-        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
-        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        # --- IMPORTANT: FILL IN YOUR DETAILS HERE ---
+        username = "admin"
+        email = "muhammedfarhant6@gmail.com"
+        password = "adminpu12"
+        # -----------------------------------------
 
         if not User.objects.filter(username=username).exists():
             self.stdout.write(f'Creating account for {username} ({email})')
             User.objects.create_superuser(email=email, username=username, password=password)
         else:
-            self.stdout.write(f'Superuser with username {username} already exists.')
+            self.stdout.write(f'Superuser with username "{username}" already exists.')
