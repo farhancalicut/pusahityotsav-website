f7a9531bc5000b33dadfdba21263992d019e2c59
farhancalicut <muhammedfarhant6@gmail.com>
2025-09-17 09:45:27 +0530

    fix: Use environment variable for API URL

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,2 +1,5 @@
-// This is the single source of truth for your backend's address
-export const API_BASE_URL = "http://127.0.0.1:8000";
+// src/apiConfig.js
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+export default API_BASE_URL;
