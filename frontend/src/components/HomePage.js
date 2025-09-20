fac60981ff95ee6bcd5cb2f2b3357654672e5993
farhancalicut <muhammedfarhant6@gmail.com>
2025-09-20 22:47:04 +0530

    fix27

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -17,7 +17,7 @@ function HomePage() {
     <>
       <Box
         sx={{
-          minHeight: "calc(80vh - 65px)",
+          minHeight: "calc(100vh - 65px)",
           backgroundColor: "#1a1a1a",
           color: "white",
           display: "flex",
